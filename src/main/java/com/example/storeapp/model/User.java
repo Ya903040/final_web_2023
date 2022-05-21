dfe305d7159588bc115054cb4c3b9b57f0908c3a
Suleiman <asirov560@gmail.com>
2022-05-21 11:28:23 +0600

    21 05 2020

--- src/main/java/com/example/storeapp/model/User.java
+++ src/main/java/com/example/storeapp/model/User.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import java.util.List;
-
+//skdnsnfksnf
 @Entity
 @Data
 @Table(name = "users")
